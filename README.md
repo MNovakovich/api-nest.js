d1aba87abcf77e2469e54b9b53e32637becc6be0
MNovakovich <marko.novakovic@mail.ru>
2020-11-13 23:36:09 +0100

    create post model & controller

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+
+### CREATE MODULE
+*  generate module  - ```nest g mo post``` 
+*  controllere      - ``` nest g co post --no-spec ```
+
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo_text.svg" width="320" alt="Nest Logo" /></a>
 </p>
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModule } from './post/post.module';
 @Module({
   imports: [
-    TypeOrmModule.forRoot()
+    TypeOrmModule.forRoot(),
+    PostModule
   ],
   controllers: [AppController],
   providers: [AppService],
